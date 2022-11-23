20e9e50c840d242c5a74b58561ab8491b9058dd0
pilarxyz <pilar@student.uns.ac.id>
2022-11-23 16:39:43 +0700

    fix database

--- ecommerce/models/order_products.py
+++ ecommerce/models/order_products.py
@@ -12,7 +12,7 @@ class Order_Products (db.Model):
     product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
     product = db.relationship('Products', backref=db.backref('order_products', lazy=True))
     quantity = db.Column(db.Integer, nullable=False)
-    size = db.Column(db.String(80), unique=True, nullable=False)
+    size = db.Column(db.String(80), unique=False, nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.now())
     updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
     deleted_at = db.Column(db.DateTime, nullable=True)
