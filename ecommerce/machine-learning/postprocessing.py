61d3e23c44377c820749a2a28c200b1ba6d47ebc
Nyoto Wijaya <107029463+nyotowijaya00@users.noreply.github.com>
2022-12-03 20:59:45 +0700

    Delete ecommerce/machine-learning directory

--- ecommerce/machine-learning/postprocessing.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-def visualization(image, label, confindence):
-    plt.title(f'{label} {confindence}')
-    plt.axis('off')
-    plt.imshow(image)
-    plt.show();
