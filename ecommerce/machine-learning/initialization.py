53303bb9d6d6956e08dc3c1bdd273bfca7c4ee87
Nyoto Wijaya <107029463+nyotowijaya00@users.noreply.github.com>
2022-12-09 10:28:38 +0700

    Update initialization.py

--- ecommerce/machine-learning/initialization.py
+++ ecommerce/machine-learning/initialization.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision.models as models
 
 class Model(nn.Module):
@@ -8,7 +9,11 @@ class Model(nn.Module):
 
         self.model = models.mobilenet_v2(pretrained=True)
         self.model.features[0][0] = nn.Conv2d(1, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
-        self.model.classifier[1] = nn.Linear(self.model.last_channel, 11)
+        self.model.classifier[1] = nn.Sequential(
+            nn.Linear(self.model.last_channel, 1000),
+            nn.ReLU(),
+            nn.Linear(1000, 11)
+        )
 
     def forward(self, x):
-        return self.model(x)
+        return F.log_softmax(self.model(x), dim=1)
